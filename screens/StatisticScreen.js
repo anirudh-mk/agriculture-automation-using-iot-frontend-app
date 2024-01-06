1ab6f5a7eda2a111ad2e140f9b56a304606dbc50
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-06 23:30:08 +0530

    feat(screen)added StaticScreen.js

---
+++ screens/StatisticScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeaddingText from '../components/HeaddingText'
+
+function StatisticScreen() {
+  return (
+    <div>
+        <HeaddingText text='Statistics'/>
+    </div>
+  )
+}
+
+export default StatisticScreen
