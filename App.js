977d9d842d0393f7ff61cf965826eb7c3639f271
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-06 10:28:02 +0530

    feat(app/app.js):screenOptions added

--- App.js
+++ App.js
@@ -7,12 +7,28 @@ import HomeScreen from './screens/HomeScreen';
 import UserScreen from './screens/UserScreen';
 
 const Tab = createBottomTabNavigator();
-
+const screenOptions = {
+  tabBarShowLabel:false,
+  headerShown:false,
+  tabBarStyle:{
+    position:"absolute",
+    bottom:0,
+    right:0,
+    left:0,
+    height:80,
+    borderTopLeftRadius:10,
+    borderTopRightRadius:10,
+    background: '#fff'
+  }
+}
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Screen1" component={HomeScreen} />
+      <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen 
+          name="Screen1" 
+          component={HomeScreen} 
+          />
         <Tab.Screen name="Screen2" component={UserScreen} />
       </Tab.Navigator>
     </NavigationContainer>
