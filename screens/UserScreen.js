406a3f7f97185b396538f19cae822c26510934ce
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-06 11:32:59 +0530

    feat(screen/UserScreen.js)Add HeaddingText in

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import HeaddingText from '../components/HeaddingText'
 
 const UserScreen = () => {
   return (
     <View>
-      <Text>UserScreen</Text>
+      <HeaddingText text='User'/>
     </View>
   )
 }
