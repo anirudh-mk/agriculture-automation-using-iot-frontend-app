8099373ba47d250e136a0c79f5a6141e77571c4b
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-08 08:56:56 +0530

    components/TextButton.js)Some property changed

--- components/TextButton.js
+++ components/TextButton.js
@@ -9,7 +9,8 @@ const TextButton = ({ color, height, width, top, border_radious, name, text_colo
             top:top, 
             borderRadius:border_radious, 
             alignItems:'center',
-            justifyContent:'center'
+            justifyContent:'center',
+            backgroundColor:color,
             }}>
             <Text style={{color:text_color}}>{name}</Text>
         </View>
