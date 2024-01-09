5e49ba17a751d2cf76cd93b9393d8815a89bfb71
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-09 16:48:43 +0530

    optim(screen/WelcomeScreen.js)

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -4,26 +4,32 @@ import CirculerImage from '../components/CirculerImage'
 import TextButton from '../components/TextButton'
 import colors from '../utils/Colors';
 
+import LinearGradient from 'react-native-linear-gradient';
+
 
 const WelcomeScreen = () => {
   return (
     <View style={styles.screen}>
-      <CirculerImage 
-        source={require('../assets/icon.png')}
-        size={330}
-        border={0}
-        top={143}
-      />
-      <TextButton
-        width={240}
-        height={59}
-        top={300}
-        color='#fff'
-        border_radious={20}
-        name='Get Started'
-        text_color={colors.secondery}
-      />
-      <Text style={styles.text}>Farming simplified Automated</Text>
+      <View style={styles.imageContainer}>
+        <CirculerImage 
+          source={require('../assets/icon.png')}
+          size={330}
+          border={0}
+          />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TextButton
+            width={240}
+            height={59}
+            color='#fff'
+            border_radious={20}
+            name='Get Started'
+            text_color={colors.secondery}
+          />
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Farming simplified Automated</Text>
+      </View>
     </View>
   )
 }
@@ -32,14 +38,28 @@ const styles = StyleSheet.create({
   screen:{
     flex:1,
     alignItems:'center',
-    backgroundColor:colors.secondery,
+    backgroundColor: 'linear-gradient(185deg, #127F00 3.84%, rgba(28, 190, 25, 0.81) 97.65%)',
+    boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+    position:'relative'
+  },
+  imageContainer:{
+    position:'absolute',
+    top:88
+  },
+  buttonContainer:{
+    position:'absolute',
+    top:609
+  },
+  textContainer:{
+    position:'absolute',
+    top:686
   },
   text:{
     color:colors.white,
     fontSize:20,
     fontWeight:'600',
-    top:350,
-  }
+    top:0,
+  },
 })
 
 export default WelcomeScreen
