4cd790828980e166debb315f805528038d8e6b06
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-09 23:07:46 +0530

    feat(screen/UserScreen.js): code optimized

--- screens/StatisticScreen.js
+++ screens/StatisticScreen.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
 import HeaddingText from '../components/HeaddingText'
 
--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,21 +1,39 @@
-import { View, Text } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import React from 'react'
+
 import HeaddingText from '../components/HeaddingText'
 import CirculerImage from '../components/CirculerImage'
+
 import colors from '../utils/Colors'
 
 const UserScreen = () => {
   return (
-    <View>
-      <HeaddingText text='User'/>
-      <CirculerImage 
-        source={require('../assets/favicon.png')} 
-        size={100} 
-        top={200}
-        border={10}
-        border_color={colors.primary}
-        />
+    <View style={ styles.screen }>
+      <View style={ styles.headdingContainer }>
+        <HeaddingText text='User'/>
+      </View>
+      <View style={ styles.imageContainer }>
+        <CirculerImage 
+          source={ require('../assets/favicon.png') } 
+          size={ 100 } 
+          border={ 10 }
+          borderColor={ colors.primary }
+          />
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  screen:{
+    flex:1,
+  },
+  headdingContainer:{
+    height:100,
+  },
+  imageContainer:{
+    top:200,
+  }
+})
+
 export default UserScreen
