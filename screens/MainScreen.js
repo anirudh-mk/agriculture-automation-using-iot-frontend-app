fb6fdcb31c50b92237a015953aba24c889f0228a
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-09 17:53:06 +0530

    feat(screen/MainScreen.js): add bottom navigation bar

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,11 +1,55 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import UserScreen from './UserScreen';
+import StatisticScreen from './StatisticScreen';
+import HomeScreen from './HomeScreen';
+const Tab = createStackNavigator();
 
 const MainScreen = () => {
   return (
-    <View>
-      <Text>MainScreen</Text>
-    </View>
+      <Tab.Navigator 
+        initialRouteName="Home"
+      >
+        <Tab.Screen 
+          name="Home" 
+          component={ HomeScreen } 
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <MaterialCommunityIcons 
+              name="home" 
+              color={ focused? colors.primary: colors.gray } 
+              size={ focused? 30: 26 } />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Statistics"
+          component={ StatisticScreen }
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <MaterialCommunityIcons 
+              name='finance' 
+              color={ focused? colors.primary: colors.gray } 
+              size={ focused? 30: 26 }/>
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="User" 
+          component={ UserScreen } 
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <MaterialCommunityIcons 
+              name="account" 
+              color={ focused? colors.primary: colors.gray } 
+              size={ focused? 30: 26} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
   )
 }
 
