e4f6478384140ff156845dda936c18e9f68dd519
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-09 22:26:32 +0530

    optim(components/headdingText.js): code optimized

--- components/HeaddingText.js
+++ components/HeaddingText.js
@@ -1,22 +1,23 @@
 import { View, Text, StyleSheet} from 'react-native'
 import React from 'react'
+import colors from '../utils/Colors'
 
-const HeaddingText = ({text}) => {
+const HeaddingText = ({ text }) => {
   return (
-    <View style={styles.text_container}>
-      <Text style={styles.text}>{text}</Text>
+    <View style={ styles.textContainer }>
+      <Text style={ styles.text }>{ text }</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    text_container:{
+    textContainer:{
         height:48,
         top:44,
         left:20
     },
     text:{
-        color:'#000',
+        color:colors.black,
         fontSize:32,
     }
 })
