af0047aeb72cf82df2b977bc6f5cf59b009af119
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-10 13:53:18 +0530

    feat(components/IconTextInput.js): styling compleated

--- components/IconTextInput.js
+++ components/IconTextInput.js
@@ -1,12 +1,71 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { TextInput } from 'react-native-gesture-handler'
 
-const IconTextInput = () => {
-  return (
-    <View>
-      <Text>IconTextInput</Text>
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import colors from '../utils/Colors';
+
+const IconTextInput = ({ width, height, icon, placeholder, color, borderRadious, isPassword }) => {
+
+    const [divWidth, setDivWidth] = useState(0)
+
+    const handleLayout = (event) => {
+        const width = event.nativeEvent.layout.width
+        setDivWidth(width)
+    }
+
+    return (
+    <View style={{ 
+        width:width, 
+        height:height,
+        backgroundColor:color,
+        borderRadius:borderRadious,
+        flexDirection:'row'
+        }}
+        onLayout={handleLayout}
+    >
+    <View style={ [styles.iconContainer, { width:height }] }>
+        <MaterialCommunityIcons 
+            name={ icon }
+            size={ 19 }
+            color={ colors.gray }
+        />
+    </View>
+    <TextInput
+        style={ [styles.textInput, { left:height, height:height, width:isPassword?divWidth-((height*2)):divWidth-(height+10)}] }
+        placeholder={ placeholder }
+    />
+    {isPassword && (
+        <View style={ [styles.passwordContainer, { width:height }] }>
+            <MaterialCommunityIcons 
+                name='eye-off-outline'
+                size={ 19 }
+                color={ colors.gray }
+            />
+        </View> 
+    )}
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    iconContainer:{
+        position:'absolute',
+        alignItems:'center',
+        justifyContent:'center',
+        height:'100%',
+        left:0,
+    },
+    passwordContainer:{
+        position:'absolute',
+        alignItems:'center',
+        justifyContent:'center',
+        height:'100%',
+        right:0,
+    },
+    textInput:{
+        position:'absolute',
+    },
+})
 
 export default IconTextInput
