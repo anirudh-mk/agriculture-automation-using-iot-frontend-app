35dba12c7139991f50c2af868054de42647ee85b
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-10 11:46:27 +0530

    feat(components): IconTextInput added

---
+++ components/IconTextInput.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const IconTextInput = () => {
+  return (
+    <View>
+      <Text>IconTextInput</Text>
+    </View>
+  )
+}
+
+export default IconTextInput
