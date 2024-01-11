e0bf51aef58abd54d9ab5ae93c015934c7bf34c2
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-11 20:06:23 +0530

    feat(screens/HomeScreen.js): new HomeScreenSupport file for handlling HomeScreen functions

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,15 +3,9 @@ import React from 'react'
 
 import ImageCard from '../components/ImageCard'
 
-const HomeScreen = () => {
+import data from './HomeScreen/HomeScreenSupportFile'
 
-  const data = [
-    { id:'1', farmName:'farm1', vegitableName:'apple', dayLeft:20},
-    {id:'2', farmName:'farm2', vegitableName:'orange', dayLeft:20},
-    {id:'3', farmName:'farm2', vegitableName:'orange', dayLeft:20},
-    {id:'4', farmName:'farm2', vegitableName:'orange', dayLeft:20},
-    {id:'5', farmName:'farm2', vegitableName:'orange', dayLeft:20}
-  ]
+const HomeScreen = () => {
 
   const renderItem = (item) => {
     return(
