c75b5da4166c94a36ce8c3595a3f4036d6d579f9
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-13 11:17:44 +0530

    optim(utils): supportive function file added

---
+++ utils/SupportiveDataFile.js
@@ -0,0 +1,41 @@
+const homeScreenFarmData = [
+    {id:'1', farmName:'farm 1', vegitableName:'Apple', dayLeft:20},
+    {id:'2', farmName:'farm 2', vegitableName:'Orange', dayLeft:12},
+    {id:'3', farmName:'farm 3', vegitableName:'Onion', dayLeft:2},
+  ]
+
+const lastCropsDetails = [
+  {id:'1', crop:'Apple', date:'12-04-2023'},
+  {id:'2', crop:'Orange', date:'01-08-2023'},
+  {id:'3', crop:'Onion', date:'04-01-2022'},
+  {id:'4', crop:'Orange', date:'01-08-2023'},
+  {id:'5', crop:'Onion', date:'04-01-2022'},
+  {id:'6', crop:'Orange', date:'01-08-2023'},
+  {id:'7', crop:'Onion', date:'04-01-2022'},
+]
+
+const pieCartData = [
+  {
+    name: "Seoul",
+    population: 65,
+    color: "#ff6347",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 15
+  },
+  {
+    name: "Toronto",
+    population: 45,
+    color: "#3779D4",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 15
+  },
+  {
+    name: "Toronto",
+    population: 35,
+    color: "#279D1E",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 15
+  },
+];
+
+export {homeScreenFarmData, lastCropsDetails, pieCartData} 
