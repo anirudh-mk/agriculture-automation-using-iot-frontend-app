1e09c20828e8d6467b9bc6e0168c460f0dec74c5
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-14 17:20:49 +0530

    feat(screens/FarmScreen.js): Scroll view added

--- screens/FarmScreen.js
+++ screens/FarmScreen.js
@@ -1,6 +1,7 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import HeaddingText from '../components/HeaddingText'
+import { ScrollView } from 'react-native-gesture-handler'
 
 const FarmScreen = ({ farmName, }) => {
   return (
@@ -8,6 +9,10 @@ const FarmScreen = ({ farmName, }) => {
         <View style={styles.headdingContainer}>
             <HeaddingText style={styles.headdingText} text='Farm 1'/>
         </View>
+        <ScrollView style={styles.scrollView}>
+          <Text style={styles.subHeadding}>Hello</Text>
+        </ScrollView>
+        <View style={styles.new}></View>
     </View>
   )
 }
@@ -15,7 +20,6 @@ const FarmScreen = ({ farmName, }) => {
 const styles = StyleSheet.create({
   screen:{
     flex:1,
-    flexDirection:'row',
     backgroundColor:'green'
   },
   headdingContainer:{
@@ -23,6 +27,16 @@ const styles = StyleSheet.create({
     height:100,
     width:'100%'
   },
+  scrollView:{
+    width:'100%',
+    backgroundColor:'yellow',
+    height:100,
+  },
+  subHeadding:{
+    padding:20,
+    fontSize:20,
+    fontWeight:'600'
+  }
 })
 
 export default FarmScreen
