26fb4b453dfefaa35136595a3f6100c8c888eb6b
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-14 17:36:53 +0530

    feat(components/DetailsCard):some props updated

--- components/DetailsCard.js
+++ components/DetailsCard.js
@@ -1,12 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import { TouchableHighlight } from 'react-native-gesture-handler'
+import { Dimensions } from 'react-native'
 
-const DetailsCard = () => {
-  return (
-    <View>
-      <Text>DetailsCard</Text>
-    </View>
+const DetailsCard = ({ marginLeft, marginRight }) => {
+
+    const { width, height } = Dimensions.get('window')
+
+    return (
+    <TouchableHighlight style={[
+        styles.card,
+        { 
+            width:width-(marginLeft+marginRight),
+             }]}>
+        <Text>hwllo</Text>
+
+    </TouchableHighlight>
   )
 }
+
+const styles = StyleSheet.create({
+    card:{
+        height:182,
+        backgroundColor:'tomato'
+    }
+})
 
 export default DetailsCard
