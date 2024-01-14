2bc76ef4ddb8c1c086de42241c8cbed32d593504
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-14 17:21:53 +0530

    feat(components/DetailsCard.js): add details card

---
+++ components/DetailsCard.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const DetailsCard = () => {
+  return (
+    <View>
+      <Text>DetailsCard</Text>
+    </View>
+  )
+}
+
+export default DetailsCard
