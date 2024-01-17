57ad54b1ac660da47ddd3b236909546c46595270
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-17 20:16:39 +0530

    optim(components/TextButton.js):code optimized

--- components/TextButton.js
+++ components/TextButton.js
@@ -1,29 +1,40 @@
-import { Text, TouchableHighlight } from 'react-native'
-import React from 'react'
+import { Text, TouchableHighlight, Dimensions } from "react-native";
+import React from "react";
 
-const TextButton = ({ color, height, width, borderRadious, name, textColor, onPress }) => {
-  
+const TextButton = ({
+  color,
+  height,
+  width,
+  borderRadious,
+  name,
+  textColor,
+  onPress,
+}) => {
+  const screenWidth = Dimensions.get("window").width;
+
   return (
-    <TouchableHighlight style={{ 
-      alignItems:'center',
-      justifyContent:'center',
-      width:width, 
-      height:height, 
-      borderRadius:borderRadious, 
-      backgroundColor:color,
+    <TouchableHighlight
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+        width: screenWidth - 40,
+        height: height,
+        borderRadius: borderRadious,
+        backgroundColor: color,
       }}
-      onPress={ onPress }
+      onPress={onPress}
     >
-        <Text style={{
-          fontSize:16,
-          fontWeight:'600',
-          color:textColor,
-          }}
-        >
-          { name }
-        </Text>
+      <Text
+        style={{
+          fontSize: 16,
+          fontWeight: "600",
+          color: textColor,
+        }}
+      >
+        {name}
+      </Text>
     </TouchableHighlight>
-  )
-}
+  );
+};
 
-export default TextButton
+export default TextButton;
