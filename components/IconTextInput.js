389526a5d174adb1cb242be410b2df7bebb35a0a
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-17 19:47:27 +0530

    optim(components/IconTextInput.js):code optimized

--- components/IconTextInput.js
+++ components/IconTextInput.js
@@ -1,63 +1,75 @@
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet } from "react-native";
+import React, { useState } from "react";
 
-import React, { useState } from 'react'
-import { TextInput } from 'react-native-gesture-handler'
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { TextInput } from "react-native-gesture-handler";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Dimensions } from "react-native";
 
-import colors from '../utils/Colors';
+import colors from "../utils/Colors";
 
-const IconTextInput = ({ width, height, icon, placeholder, color, borderRadious, onChangeText }) => {
+const IconTextInput = ({
+  height,
+  icon,
+  placeholder,
+  color,
+  borderRadious,
+  onChangeText,
+}) => {
+  const screenWidth = Dimensions.get("window").width;
+  const [containerWidth, setContainerWidth] = useState(0);
 
-    const [divWidth, setDivWidth] = useState(0)
-    
-    const handleLayout = (event) => {
-        const width = event.nativeEvent.layout.width
-        setDivWidth(width)
-    }
+  const handleLayout = (event) => {
+    const width = event.nativeEvent.layout.width;
+    setContainerWidth(width);
+  };
 
-    return (
-    <View style={[styles.screen, { 
-        width:width, 
-        height:height,
-        backgroundColor:color,
-        borderRadius:borderRadious,
-        }] }
-        onLayout={handleLayout}
+  return (
+    <View
+      style={[
+        styles.screen,
+        {
+          width: screenWidth - 40,
+          height: height,
+          backgroundColor: color,
+          borderRadius: borderRadious,
+        },
+      ]}
+      onLayout={handleLayout}
     >
-    <View style={ [styles.iconContainer, { width:height }] }>
-        <MaterialCommunityIcons 
-            name={ icon }
-            size={ 19 }
-            color={ colors.gray }
-        />
+      <View style={[styles.iconContainer, { width: height }]}>
+        <MaterialCommunityIcons name={icon} size={20} color={colors.gray} />
+      </View>
+      <TextInput
+        style={[
+          styles.textInput,
+          {
+            left: height,
+            height: height,
+            width: containerWidth - (height + 10),
+          },
+        ]}
+        placeholder={placeholder}
+        onChangeText={onChangeText}
+      />
     </View>
-    <TextInput
-        style={ [styles.textInput, { 
-            left:height, 
-            height:height, 
-            width:divWidth-(height+10)}] }
-        placeholder={ placeholder }
-        onChangeText={ onChangeText }
-    />
-    </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    screen:{
-        flexDirection:'row',
-        position:'relative'
-    },
-    iconContainer:{
-        position:'absolute',
-        alignItems:'center',
-        justifyContent:'center',
-        height:'100%',
-        left:0,
-    },
-    textInput:{
-        position:'absolute',
-    },
-})
+  screen: {
+    flexDirection: "row",
+    position: "relative",
+  },
+  iconContainer: {
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    left: 0,
+  },
+  textInput: {
+    position: "absolute",
+  },
+});
 
-export default IconTextInput
+export default IconTextInput;
