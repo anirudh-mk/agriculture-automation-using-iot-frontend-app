85ecd9852488e5c2fb93a96da45933462821b541
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-17 20:15:53 +0530

    optim(components/IconTextButton.js):code optimized

--- components/IconTextButton.js
+++ components/IconTextButton.js
@@ -1,61 +1,88 @@
-import { Text, TouchableHighlight, View, StyleSheet} from 'react-native'
-import React from 'react'
+import {
+  Text,
+  TouchableHighlight,
+  View,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
+import React from "react";
 
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import colors from '../utils/Colors';
+import colors from "../utils/Colors";
 
-const TextButton = ({ color, height, width, borderRadious, name, textColor, onPress, icon, textPosition, marginBottom, iconColor }) => {
-  
+const TextButton = ({
+  color,
+  height,
+  width,
+  borderRadious,
+  name,
+  textColor,
+  onPress,
+  icon,
+  textPosition,
+  marginBottom,
+  iconColor,
+}) => {
+  const screenWidth = Dimensions.get("window").width;
+
   return (
-    <TouchableHighlight style={ [styles.screen, { 
-      width:width, 
-      height:height, 
-      borderRadius:borderRadious, 
-      backgroundColor:color,
-      marginBottom:marginBottom?marginBottom:0,
-      }] }
-      onPress={ onPress }
+    <TouchableHighlight
+      style={[
+        styles.screen,
+        {
+          width: screenWidth - 40,
+          height: height,
+          borderRadius: borderRadious,
+          backgroundColor: color,
+          marginBottom: marginBottom ? marginBottom : 0,
+        },
+      ]}
+      onPress={onPress}
     >
       <View style={styles.iconContainer}>
-          <MaterialCommunityIcons
-            style={styles.iconStyle}
-            name={ icon }
-            size={ 20 }
-            color={ iconColor?iconColor:colors.black }
-          />
-          <Text style={[styles.text,{
-            color:textColor,
-            textAlign:textPosition?textPosition:'center',
-          }]}
-          >
-          { name }
-          </Text>
+        <MaterialCommunityIcons
+          style={styles.iconStyle}
+          name={icon}
+          size={20}
+          color={iconColor ? iconColor : colors.black}
+        />
+        <Text
+          style={[
+            styles.text,
+            {
+              color: textColor,
+              textAlign: textPosition ? textPosition : "center",
+            },
+          ]}
+        >
+          {name}
+        </Text>
       </View>
     </TouchableHighlight>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  screen:{
-    position:'relative',
-    justifyContent:'center',
+  screen: {
+    position: "relative",
+    justifyContent: "center",
   },
-  text:{
-    width:'100%',
-    paddingRight:20,
-    paddingLeft:20,
-    fontSize:16,
-    fontWeight:'600',
+  text: {
+    width: "100%",
+    paddingRight: 20,
+    paddingLeft: 20,
+    fontSize: 16,
+    fontWeight: "600",
   },
-  iconContainer:{
-    position:'absolute', 
-    width:'100%', 
-    flexDirection:'row',
+  iconContainer: {
+    position: "absolute",
+    width: "100%",
+    flexDirection: "row",
   },
-  iconStyle:{
-    position:'absolute',
-    left:12, 
-  }
-})
-export default TextButton
+  iconStyle: {
+    position: "absolute",
+    left: 12,
+  },
+});
+export default TextButton;
