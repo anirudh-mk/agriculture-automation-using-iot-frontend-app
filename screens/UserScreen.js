978718365ba7aa6b591a838ff36b408e8ecc2e6d
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-28 11:27:35 +0530

    optim(UserScreen.jsx)

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,122 +1,125 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
-
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useNavigation } from '@react-navigation/native';
-
-import IconTextButton from '../components/IconTextButton'
-import HeaddingText from '../components/HeaddingText'
-import CirculerImage from '../components/CirculerImage'
-import colors from '../utils/Colors'
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
+import IconTextButton from "../components/IconTextButton";
+import CirculerImage from "../components/CirculerImage";
+import colors from "../utils/Colors";
 
 const UserScreen = () => {
   const navigation = useNavigation();
 
-  const logout = async => {
-    try{
-      AsyncStorage.removeItem('isLogin')
-      navigation.navigate("loginScreen")
-    } catch(error){
-      console.log(error)
+  const handleLogout = (async) => {
+    try {
+      AsyncStorage.removeItem("isLogin");
+      navigation.navigate("loginScreen");
+    } catch (error) {
+      console.log(error);
     }
-  }
+  };
 
   return (
     <View style={styles.screen}>
       <View style={styles.headdingContainer}>
-        <HeaddingText text='User'/>
+        <Text style={styles.headdingText}>User</Text>
       </View>
-      <View style={ styles.imageContainer }>
-        <CirculerImage 
-          source={ require('../assets/user.png') } 
-          size={ 121 } 
-          border={ 0 }
-          borderColor={ colors.gray }
-          />
-          <Text style={styles.userName}>Anirudh MK</Text>
-          <Text style={styles.userEmail}>anirudhmk123@gmail.com</Text>
+      <View style={styles.imageContainer}>
+        <CirculerImage
+          source={require("../assets/user.png")}
+          size={121}
+          border={0}
+          borderColor={colors.gray}
+        />
+        <Text style={styles.userName}>Anirudh MK</Text>
+        <Text style={styles.userEmail}>anirudhmk123@gmail.com</Text>
       </View>
       <View style={styles.buttonContainer}>
         <IconTextButton
-          color={ colors.white }
-          height={ 60 }
-          width='90%'
-          borderRadious={ 12 }
-          name='User'
-          textColor={ colors.black }
-          icon='at'
-          textPosition='right'
-          marginBottom={ 20 }
+          color={colors.white}
+          height={60}
+          width="90%"
+          borderRadious={12}
+          name="User"
+          textColor={colors.black}
+          icon="at"
+          textPosition="right"
+          marginBottom={20}
         />
         <IconTextButton
-          color={ colors.white }
-          height={ 60 }
-          width='90%'
-          borderRadious={ 12 }
-          name='Contact us'
-          textColor={ colors.black }
-          icon='message'
-          textPosition='right'
-          marginBottom={ 20 }
+          color={colors.white}
+          height={60}
+          width="90%"
+          borderRadious={12}
+          name="Contact us"
+          textColor={colors.black}
+          icon="message"
+          textPosition="right"
         />
       </View>
       <View style={styles.logoutContainer}>
         <IconTextButton
-          color={ colors.white }
-          height={ 60 }
-          width='90%'
-          borderRadious={ 12 }
-          name='Logout'
-          textColor={ colors.tomato }
-          icon='logout'
-          textPosition='right'
-          marginBottom={ 20 }
-          iconColor={ colors.tomato }
-          onPress={ logout }
+          color={colors.white}
+          height={60}
+          width="90%"
+          borderRadious={12}
+          name="Logout"
+          textColor={colors.tomato}
+          icon="logout"
+          textPosition="right"
+          iconColor={colors.tomato}
+          onPress={handleLogout}
         />
       </View>
     </View>
-    )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  screen:{
-    flex:1,
-    position:'relative',
-    paddingBottom:100,
+  screen: {
+    flex: 1,
+    flexDirection: "column",
+    paddingBottom: 80,
+    paddingTop: 24,
   },
-  headdingContainer:{
-    position:'absolute',
-    height:100,
-    width:'100%',
+  headdingContainer: {
+    display: "flex",
+    height: 80,
+    width: "100%",
+    justifyContent: "center",
+    paddingLeft: 20,
   },
-  imageContainer:{
-    position:'absolute',
-    alignItems:'center',
-    top:155,
-    width:'100%',
+  headdingText: {
+    fontSize: 28,
+    fontWeight: "600",
+    color: colors.primary,
   },
-  userName:{
-    position:'absolute',
-    top:131,
-    fontSize:12,
-    fontWeight:'600',
-  },
-  userEmail:{
-    position:'absolute',
-    top:154,
-    fontSize:10,
-    fontWeight:'600',
-    color:colors.gray,
-  },
-  buttonContainer:{
-    top:375,
-    alignItems:'center'
-  },
-  logoutContainer:{
-    top:500,
-    alignItems:'center'
-  }
-})
+  imageContainer: {
+    width: "100%",
 
-export default UserScreen
+    paddingBottom: 20,
+    alignItems: "center",
+  },
+  userName: {
+    paddingTop: 10,
+    fontSize: 12,
+    fontWeight: "600",
+  },
+  userEmail: {
+    paddingTop: 5,
+    fontSize: 10,
+    fontWeight: "600",
+    color: colors.gray,
+  },
+  buttonContainer: {
+    flex: 1,
+    paddingTop: 20,
+    alignItems: "center",
+  },
+  logoutContainer: {
+    height: 100,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default UserScreen;
