d4c5c256b2f88c852ebd9f5248ed11a275f65637
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-28 11:48:47 +0530

    optim(MainScreen.js):name changed

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,52 +1,48 @@
-import { View, StyleSheet, FlatList } from 'react-native'
-import React from 'react'
-
-import { useNavigation } from '@react-navigation/native'
-
-import ImageCard from '../components/ImageCard'
-
-import { homeScreenFarmData } from '../utils/SupportiveDataFile'
+import { View, StyleSheet, FlatList } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import ImageCard from "../components/ImageCard";
+import { homeScreenFarmData } from "../utils/SupportiveDataFile";
 
 const HomeScreen = () => {
-
   const navigation = useNavigation();
 
   const navigate = () => {
-    navigation.navigate('farmScreen')
-  }
+    navigation.navigate("farmScreen");
+  };
 
   const renderItem = (item) => {
-    return(
-      <ImageCard 
-        banner={ require('../assets/tomato.png') }
-        farm={ item.item.farmName }
-        vegitable={ item.item.vegitableName }
-        daysLeft={ item.item.dayLeft + 'day left' }
-        onPress={ navigate }
+    return (
+      <ImageCard
+        banner={require("../assets/tomato.png")}
+        farm={item.item.farmName}
+        vegitable={item.item.vegitableName}
+        daysLeft={item.item.dayLeft + "day left"}
+        onPress={navigate}
       />
-    )
-  }
+    );
+  };
 
   return (
-    <View style={ styles.screen }>
+    <View style={styles.screen}>
       <FlatList
-        style={{ width:'100%' }}
-        contentContainerStyle={{ alignItems:'center' }}
-        data={ homeScreenFarmData }
-        renderItem={ renderItem }
-        keyExtractor={ (item)=> item.id }
+        style={{ width: "100%" }}
+        contentContainerStyle={{ alignItems: "center" }}
+        data={homeScreenFarmData}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
       />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  screen:{
-    flex:1,
-    alignItems:'center',
-    paddingTop:25,
-    paddingBottom:80,
-  }
-})
+  screen: {
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 25,
+    paddingBottom: 80,
+  },
+});
 
-export default HomeScreen
+export default HomeScreen;
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,72 +1,74 @@
-import React from 'react';
+import React from "react";
 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import HomeScreen from './HomeScreen';
-import UserScreen from './UserScreen';
-import StatisticScreen from './StatisticScreen';
+import HomeScreen from "./HomeScreen";
+import UserScreen from "./UserScreen";
+import StatisticScreen from "./StatisticScreen";
 
-import colors from '../utils/Colors';
+import colors from "../utils/Colors";
 
 const Tab = createBottomTabNavigator();
 const screenOptions = {
-  tabBarShowLabel:false,
-  headerShown:false,
-  tabBarStyle:{
-    position:"absolute",
-    bottom:0,
-    right:0,
-    left:0,
-    height:80,
-    borderTopLeftRadius:10,
-    borderTopRightRadius:10,
-    background: '#fff'
-  }
-}
-const App = () => {
+  tabBarShowLabel: false,
+  headerShown: false,
+  tabBarStyle: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    left: 0,
+    height: 80,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    background: "#fff",
+  },
+};
+
+const MainScreen = () => {
   return (
-     <Tab.Navigator 
-        initialRouteName="Home"
-        screenOptions={screenOptions}
-      >
-        <Tab.Screen 
-          name="Home" 
-          component={ HomeScreen } 
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <MaterialCommunityIcons 
-              name="home" 
-              color={ focused? colors.primary: colors.gray } 
-              size={ focused? 30: 26 } />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Statistics"
-          component={ StatisticScreen }
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <MaterialCommunityIcons 
-              name='finance' 
-              color={ focused? colors.primary: colors.gray } 
-              size={ focused? 30: 26 }/>
-            ),
-          }}
-        />
-        <Tab.Screen 
-          name="User" 
-          component={ UserScreen } 
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <MaterialCommunityIcons 
-              name="account" 
-              color={ focused? colors.primary: colors.gray } 
-              size={ focused? 30: 26} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
+    <Tab.Navigator initialRouteName="Home" screenOptions={screenOptions}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons
+              name="home"
+              color={focused ? colors.primary : colors.gray}
+              size={focused ? 30 : 26}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Statistics"
+        component={StatisticScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons
+              name="finance"
+              color={focused ? colors.primary : colors.gray}
+              size={focused ? 30 : 26}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="User"
+        component={UserScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons
+              name="account"
+              color={focused ? colors.primary : colors.gray}
+              size={focused ? 30 : 26}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 };
-export default App;
+
+export default MainScreen;
