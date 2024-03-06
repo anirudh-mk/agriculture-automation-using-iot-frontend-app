6043fe9a507f01a57b082c9f5eb6bf554257df52
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-03-06 20:26:53 +0530

    optim(screens\UserScreen.js)

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,22 +1,22 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useNavigation } from "@react-navigation/native";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+// import { useNavigation } from "@react-navigation/native";
 import IconTextButton from "../components/IconTextButton";
 import CirculerImage from "../components/CirculerImage";
 import colors from "../utils/Colors";
 
 const UserScreen = () => {
-  const navigation = useNavigation();
+  // const navigation = useNavigation();
 
-  const handleLogout = (async) => {
-    try {
-      AsyncStorage.removeItem("isLogin");
-      navigation.navigate("loginScreen");
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const handleLogout = (async) => {
+  //   try {
+  //     AsyncStorage.removeItem("isLogin");
+  //     navigation.navigate("loginScreen");
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   return (
     <View style={styles.screen}>
@@ -26,48 +26,25 @@ const UserScreen = () => {
       <View style={styles.imageContainer}>
         <CirculerImage
           source={require("../assets/user.png")}
-          size={121}
-          border={0}
-          borderColor={colors.gray}
+          size={150}
         />
         <Text style={styles.userName}>Anirudh MK</Text>
         <Text style={styles.userEmail}>anirudhmk123@gmail.com</Text>
       </View>
       <View style={styles.buttonContainer}>
         <IconTextButton
-          color={colors.white}
-          height={60}
-          width="90%"
-          borderRadious={12}
           name="User"
-          textColor={colors.black}
           icon="at"
-          textPosition="right"
-          marginBottom={20}
         />
         <IconTextButton
-          color={colors.white}
-          height={60}
-          width="90%"
-          borderRadious={12}
           name="Contact us"
-          textColor={colors.black}
           icon="message"
-          textPosition="right"
         />
       </View>
       <View style={styles.logoutContainer}>
         <IconTextButton
-          color={colors.white}
-          height={60}
-          width="90%"
-          borderRadious={12}
           name="Logout"
-          textColor={colors.tomato}
           icon="logout"
-          textPosition="right"
-          iconColor={colors.tomato}
-          onPress={handleLogout}
         />
       </View>
     </View>
@@ -83,10 +60,7 @@ const styles = StyleSheet.create({
   },
   headdingContainer: {
     display: "flex",
-    height: 80,
-    width: "100%",
-    justifyContent: "center",
-    paddingLeft: 20,
+    padding: 20
   },
   headdingText: {
     fontSize: 28,
@@ -94,29 +68,29 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     width: "100%",
-    paddingBottom: 20,
+    padding: 10,
     alignItems: "center",
   },
   userName: {
     paddingTop: 10,
-    fontSize: 12,
+    fontSize: 20,
     fontWeight: "600",
   },
   userEmail: {
-    paddingTop: 5,
-    fontSize: 10,
+    padding: 5,
+    fontSize: 16,
     fontWeight: "600",
     color: colors.gray,
   },
   buttonContainer: {
     flex: 1,
-    paddingTop: 20,
+    padding: 20,
     alignItems: "center",
+
   },
   logoutContainer: {
-    height: 100,
+    padding: 20,
     alignItems: "center",
-    justifyContent: "center",
   },
 });
 
