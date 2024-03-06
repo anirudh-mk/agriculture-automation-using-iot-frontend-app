c6e46a7b3a347c5dc43f0d5de1b118724fa0fe59
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-03-06 20:28:25 +0530

    app

--- App.js
+++ App.js
@@ -1,46 +1,61 @@
-import React from "react";
+// import React from "react";
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from "@react-navigation/stack";
 
-import MainScreen from "./screens/MainScreen";
-import WelcomeScreen from "./screens/WelcomeScreen";
-import LoginScreen from "./screens/LoginScreen";
-import FarmScreen from "./screens/FarmScreen";
+// import MainScreen from "./screens/MainScreen";
+// import WelcomeScreen from "./screens/WelcomeScreen";
+// import LoginScreen from "./screens/LoginScreen";
+// import FarmScreen from "./screens/FarmScreen";
 
-const screenOptions = {
-  tabBarShowLabel: false,
-  headerShown: false,
-  tabBarStyle: {
-    position: "absolute",
-    bottom: 0,
-    right: 0,
-    left: 0,
-    height: 80,
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    background: "#fff",
-  },
-};
+// const screenOptions = {
+//   tabBarShowLabel: false,
+//   headerShown: false,
+//   tabBarStyle: {
+//     position: "absolute",
+//     bottom: 0,
+//     right: 0,
+//     left: 0,
+//     height: 80,
+//     borderTopLeftRadius: 10,
+//     borderTopRightRadius: 10,
+//     background: "#fff",
+//   },
+// };
 
-const Screen = createStackNavigator();
+// const Screen = createStackNavigator();
+
+// const App = () => {
+//   return (
+//     <>
+//       <NavigationContainer>
+//         <Screen.Navigator
+//           initialRouteName="welcomeScreen"
+//           screenOptions={screenOptions}
+//         >
+//           <Screen.Screen name="welcomeScreen" component={WelcomeScreen} />
+//           <Screen.Screen name="loginScreen" component={LoginScreen} />
+//           <Screen.Screen name="mainScreen" component={MainScreen} />
+//           <Screen.Screen name="farmScreen" component={FarmScreen} />
+//         </Screen.Navigator>
+//       </NavigationContainer>
+//     </>
+//   );
+// };
+
+// export default App;
+
+import { View, Text } from 'react-native'
+import React from 'react'
+import WelcomeScreen from './screens/WelcomeScreen'
+import UserScreen from './screens/UserScreen'
+
 
 const App = () => {
   return (
-    <>
-      <NavigationContainer>
-        <Screen.Navigator
-          initialRouteName="welcomeScreen"
-          screenOptions={screenOptions}
-        >
-          <Screen.Screen name="welcomeScreen" component={WelcomeScreen} />
-          <Screen.Screen name="loginScreen" component={LoginScreen} />
-          <Screen.Screen name="mainScreen" component={MainScreen} />
-          <Screen.Screen name="farmScreen" component={FarmScreen} />
-        </Screen.Navigator>
-      </NavigationContainer>
-    </>
-  );
-};
+    // <WelcomeScreen />
+    <UserScreen />
+  )
+}
 
-export default App;
+export default App
