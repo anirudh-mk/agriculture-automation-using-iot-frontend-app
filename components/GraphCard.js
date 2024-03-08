e4ff3272422144118a699fe42320c345b13b4566
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-03-08 14:13:01 +0530

    optim:(components\GraphCard.js)

--- components/GraphCard.js
+++ components/GraphCard.js
@@ -1,103 +1,132 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableHighlight, Button, TouchableOpacity, TouchableOpacityBase } from 'react-native'
+import React, { useState } from 'react'
 
 import { Dimensions } from 'react-native'
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
-import { TouchableHighlight } from 'react-native-gesture-handler'
-import colors from '../utils/Colors'
 import {
   LineChart,
 } from "react-native-chart-kit";
 
-const DetailsCard = ({ marginLeft, marginRight, borderRadius, vagiatableName }) => {
+const DetailsCard = ({ vagiatableName, navigation }) => {
 
-    const { width, height } = Dimensions.get('window')
+  const [active, setActive] = useState('npk')
 
-    const screenWidth = Dimensions.get("window").width;
 
-    const chartConfig = {
-      backgroundGradientFrom: "#1E2923",
-      backgroundGradientFromOpacity: 0,
-      backgroundGradientTo: "#08130D",
-      backgroundGradientToOpacity: 0.5,
-      color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-      strokeWidth: 2, // optional, default 3
-      barPercentage: 0.5,
-      useShadowColorFromDataset: false // optional
-    };
+  const screenWidth = Dimensions.get("window").width;
+  const chartConfig = {
+    backgroundGradientFrom: "#fff",
+    backgroundGradientToOpacity: 0,
+    color: (opacity = 1) => `rgba(43,105,198, ${opacity})`,
+  };
 
-    const data = {
-      labels: ["January", "February", "March", "April", "May", "June"],
-      datasets: [
-        {
-          data: [20, 45, 28, 80, 99, 43],
-          color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
-          strokeWidth: 2 // optional
-        }
-      ],
-      legend: ["Rainy Days"] // optional
-    };
+  const data = {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    datasets: [
+      {
+        data: [90, 35, 34, 80, 99, 43, 54],
+        color: (opacity = 1) => `rgba(43, 105, 198, ${opacity})`,
+        strokeWidth: 2
+      },
+      {
+        data: [46, 67, 78, 43, 87, 43, 86],
+        color: (opacity = 1) => `rgba(108, 82, 184, ${opacity})`,
+        strokeWidth: 2
+      }
+    ]
+  };
 
-    return (
-    <TouchableHighlight style={[
-        styles.card,
-        {
-            width:width-(marginLeft+marginRight),
-            borderRadius:borderRadius,
-             }]}>
-        <View>
-          <LineChart
-            data={data}
-            width={screenWidth}
-            height={256}
-            verticalLabelRotation={30}
-            chartConfig={chartConfig}
-            bezier
-          />
+  return (
+    <TouchableHighlight style={[styles.card, navigation ? { paddingTop: 0 } : { paddingTop: 20 }]}>
+      <View>
+        {navigation && <View style={styles.menuContainer}>
+          <TouchableOpacity style={styles.button} onPress={() => setActive('npk')}>
+            <View>
+              <View style={[styles.menuActiveItem, active === 'npk' ? { backgroundColor: '#076DFC' } : { backgroundColor: 'white' }]} />
+              <Text style={styles.buttonText}>NPK</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => setActive('n')}>
+            <View>
+              <View style={[styles.menuActiveItem, active === 'n' ? { backgroundColor: '#076DFC' } : { backgroundColor: 'white' }]} />
+              <Text style={styles.buttonText}>N</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => setActive('p')}>
+            <View>
+              <View style={[styles.menuActiveItem, active === 'p' ? { backgroundColor: '#076DFC' } : { backgroundColor: 'white' }]} />
+              <Text style={styles.buttonText}>P</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => setActive('k')}>
+            <View>
+              <View style={[styles.menuActiveItem, active === 'k' ? { backgroundColor: '#076DFC' } : { backgroundColor: 'white' }]} />
+              <Text style={styles.buttonText}>K</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => setActive('t')}>
+            <View>
+              <View style={[styles.menuActiveItem, active === 't' ? { backgroundColor: '#076DFC' } : { backgroundColor: 'white' }]} />
+              <Text style={styles.buttonText}>T</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => setActive('w')}>
+            <View>
+              <View style={[styles.menuActiveItem, active === 'w' ? { backgroundColor: '#076DFC' } : { backgroundColor: 'white' }]} />
+              <Text style={styles.buttonText}>W</Text>
+            </View>
+          </TouchableOpacity>
         </View>
-    </TouchableHighlight>
+        }
+
+        <LineChart
+          data={data}
+          width={screenWidth}
+          height={200}
+          verticalLabelRotation={0}
+          chartConfig={chartConfig}
+          bezier
+          withDots={false}
+          withInnerLines={false}
+          withOuterLines={false}
+          fromZero={true}
+          formatYLabel={(label) => label | 0}
+        />
+      </View>
+    </TouchableHighlight >
   )
 }
 
 const styles = StyleSheet.create({
-    card:{
-        height:233,
-        borderWidth: 1,
-        borderColor: 'rgba(10, 109, 247, 0.19)',
-        backgroundColor: 'rgba(250, 250, 250, 0.00)',
-        shadowColor: 'rgba(38, 110, 211, 0.24)',
-        shadowOffset: { width: 0, height: 4 },
-        shadowOpacity: 1,
-        shadowRadius: 10,
-    },
-    heddingText:{
-        fontSize:12,
-        fontWeight:'600',
-        top:27,
-        left:20,
-    },
-    icon:{
-        top:50,
-        left:20,
-        position:'absolute'
-    },
-    subHeadding:{
-        position:'absolute',
-        top:50,
-        left:36,
-        fontSize:10,
-        color:colors.gray
-
-    },
-    grapContainer:{
-        position:'absolute',
-        backgroundColor:'tomato',
-        top:80,
-        width:'100%',
-        height:100,
-        flex:1,
-        flexDirection:'row'
-    }
-})
+  card: {
+    display: 'flex',
+    alignItems: 'center',
+    width: '100%',
+    borderWidth: 1,
+    borderColor: 'rgba(10, 109, 247, 0.19)',
+    backgroundColor: 'rgba(250, 250, 250, 0.00)',
+    shadowColor: 'rgba(38, 110, 211, 0.24)',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 1,
+    shadowRadius: 10,
+    borderRadius: 10,
+    paddingBottom: 20
+  },
+  menuContainer: {
+    paddingRight: 20,
+    display: 'flex',
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  buttonText: {
+    fontSize: 14,
+    fontWeight: '500',
+    padding: 10
+  },
+  menuActiveItem: {
+    height: 4,
+    width: '100%',
+  }
+}
+)
 
 export default DetailsCard
