9fcce87d02451671640249824d508b1d1e496bda
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-03-08 14:14:42 +0530

    optim:(screens\FarmScreen.js)

--- screens/FarmScreen.js
+++ screens/FarmScreen.js
@@ -1,69 +1,63 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import React from 'react'
-import HeaddingText from '../components/HeaddingText'
-import { ScrollView } from 'react-native-gesture-handler'
 import DetailsCard from '../components/DetailsCard'
 import GraphCard from '../components/GraphCard'
-
-const FarmScreen = ({ farmName, }) => {
+const FarmScreen = () => {
   return (
     <View style={styles.screen}>
-        <View style={styles.headdingContainer}>
-            <HeaddingText style={styles.headdingText} text='Farm 1'/>
+      <View style={styles.headdingContainer}>
+        <Text style={styles.headdingText}>Farm 1</Text>
+      </View>
+      <ScrollView style={styles.scrollViewContainer}>
+        <Text style={styles.subHeadding}>NPK</Text>
+        <View style={styles.deailsCardContainer}>
+          <DetailsCard
+            vagiatableName='Onion'
+          />
         </View>
-        <ScrollView style={styles.scrollView}>
-          <Text style={styles.subHeadding}>NPK</Text>
-          <View style={styles.deailsCardContainer}>
-            <DetailsCard
-              marginLeft={18}
-              marginRight={18}
-              borderRadius={10}
-              vagiatableName='Onion'
-            />
-          </View>
-          <Text style={styles.subHeadding}>Live Data</Text>
-          <View style={styles.deailsCardContainer}>
-            <GraphCard
-              marginLeft={18}
-              marginRight={18}
-              borderRadius={10}
-              vagiatableName='Onion'
-            />
-          </View>
-          <Text style={styles.subHeadding}>Market Prediction</Text>
-          <View style={styles.deailsCardContainer}>
-            <GraphCard
-              marginLeft={18}
-              marginRight={18}
-              borderRadius={10}
-              vagiatableName='Onion'
-            />
-          </View>
-        </ScrollView>
-        <View style={styles.new}></View>
+        <Text style={styles.subHeadding}>Live Data</Text>
+        <View style={styles.deailsCardContainer}>
+          <GraphCard
+            vagiatableName='Onion'
+            navigation={true}
+          />
+        </View>
+        <Text style={styles.subHeadding}>Market Prediction</Text>
+        <View style={styles.deailsCardContainer}>
+          <GraphCard
+            vagiatableName='Onion'
+          />
+        </View>
+      </ScrollView>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  screen:{
-    flex:1,
+  screen: {
+    display: "flex",
+    flex: 1,
+    paddingTop: 24,
   },
-  headdingContainer:{
-    height:100,
-    width:'100%'
+  headdingContainer: {
+    padding: 20,
   },
-  scrollView:{
-    width:'100%',
-    height:100,
+  headdingText: {
+    fontSize: 28,
+    fontWeight: "600",
   },
-  subHeadding:{
-    padding:20,
-    fontSize:20,
-    fontWeight:'600'
+  scrollViewContainer: {
+    width: '100%',
+    flex: 1
   },
-  deailsCardContainer:{
-    alignItems:'center'
+  subHeadding: {
+    padding: 20,
+    fontSize: 20,
+    fontWeight: '600'
+  },
+  deailsCardContainer: {
+    alignItems: 'center',
+    padding: 20
   },
 })
 
